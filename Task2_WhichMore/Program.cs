1b6b34c0315cdda6d06991577b47644f8a5f20ee
Lubov <pitisyka@yandex.ru>
2022-08-06 17:29:55 +0500

    task 2

---
+++ Task2_WhichMore/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите два числа:");
+double numberA = double.Parse(Console.ReadLine());
+double numberB = double.Parse(Console.ReadLine());
+if (numberA > numberB) 
+{
+    Console.WriteLine($"{numberA} > {numberB}");
+}
+else if (numberA < numberB) 
+{
+    Console.WriteLine($"{numberA} < {numberB}");
+}
+else 
+{
+    Console.WriteLine($"{numberA} = {numberB}");
+}

a66541dd6f519d470a7d784b8c10b2654abe9f55
Lubov <pitisyka@yandex.ru>
2022-08-06 17:53:01 +0500

    Task6

---
+++ Task6_Evennes/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+}

064de84a0fd7524115436abc4c9fdc9392e26875
Lubov <pitisyka@yandex.ru>
2022-08-06 17:39:56 +0500

    Task4

---
+++ Task4_Max/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите три числа:");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.Write($"Наибольшее число {max}");

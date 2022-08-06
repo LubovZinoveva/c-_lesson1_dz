a7d2fb57789ffe1c896e29390089ec3318462b39
Lubov <pitisyka@yandex.ru>
2022-08-06 18:19:38 +0500

    Task 8

---
+++ Task8_AllEven/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numberN = int.Parse(Console.ReadLine());
+int i = 2;
+String result = "";
+while (i <= numberN)
+{
+    result += Convert.ToString(i);
+    result += " ";
+    i += 2;
+}
+Console. Write(result);

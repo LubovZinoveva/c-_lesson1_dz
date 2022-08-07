613eb260c6fbcbeed912ca86954fe7212dcd671c
Lubov <pitisyka@yandex.ru>
2022-08-07 16:03:52 +0500

    The if loop was changed to switch-case

--- Task3_Week/Program.cs
+++ Task3_Week/Program.cs
@@ -1,35 +1,29 @@
 ﻿Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-if (number == 1)
+switch (number)
 {
-    Console.WriteLine("Понедельник");
-}
-
-else if (number == 2)
-{
-    Console.WriteLine("Вторник");
-}
-else if (number == 3)
-{
-    Console.WriteLine("Среда");
-}
-else if (number == 4)
-{
-    Console.WriteLine("Четверг");
-}
-else if (number == 5)
-{
-    Console.WriteLine("Пятница");
-}
-else if (number == 6)
-{
-    Console.WriteLine("Суббота");
-}
-else if (number == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
-else 
-{
-    Console.WriteLine("Неверное число, ввидите цифру от 1 до 7");
+    case 1: 
+        Console.WriteLine("Понедельник");
+        break;
+    case 2: 
+        Console.WriteLine("Вторник");
+        break;
+    case 3: 
+        Console.WriteLine("Среда");
+        break;
+    case 4: 
+        Console.WriteLine("Четверг");
+        break;
+    case 5: 
+        Console.WriteLine("Пятница");
+        break;
+    case 6: 
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default: 
+        Console.WriteLine("Неверное число, ввидите цифру от 1 до 7");
+        break;
 }
